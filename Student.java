952ad4ae9f0fb41b1d0ca6e22352bb3fa90b52b5
alexlee5124 <59184872+alexlee5124@users.noreply.github.com>
2022-04-16 11:13:02 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
  * @version 4/5/2022
  */
 public class Student extends Account {
+    Tools tools = new Tools();
 
     public Student( String username, boolean isLogged) throws FileNotFoundException {
         super( username, isLogged );
@@ -23,34 +24,28 @@ public class Student extends Account {
      * @version 4/10/2022
      */
     public Quiz[] retrieveQuizzes() {
-        ArrayList<String> quizList = new ArrayList<String>();
-        try (BufferedReader bfr = new BufferedReader(new FileReader("Quiz.txt"))) {
-            String line;
-            while ((line = bfr.readLine()) != null) {
-                quizList.add(line);
-            }
-        } catch (IOException ie) {
-            System.out.println("Either the file doesn't exist or the file is in the wrong format!");
-        }
-        Quiz[] quizzes = new Quiz[quizList.size()];
-        for ( int i = 0 ; i < quizList.size() ; i++ ) {
-            int quizID = Integer.parseInt((quizList.get(i).split(":"))[0]);
-            int numberQuestions = Integer.parseInt((quizList.get(i).split(":"))[1]);
+        String quizFile = tools.loadTextFile("Quiz.txt");
+        String[] lines = quizFile.split("/");
+        Quiz[] quizzes = new Quiz[lines.length];
+        for ( int i = 0 ; i < lines.length ; i++ ) {
+            String[] elements = lines[i].split(":");
+            int quizID = Integer.parseInt((elements[0]));
+            int numberQuestions = Integer.parseInt(elements[1]);
             int[] questionPoints = new int[numberQuestions];
             for ( int j = 0 ; j < numberQuestions ; j++ ) {
-                questionPoints[j] = Integer.parseInt(((quizList.get(i).split(":"))[2]).split(",")[j]);
+                questionPoints[j] = Integer.parseInt((elements[2]).split(",")[j]);
             }
-            String deadlineString = (quizList.get(i).split(":")[3]);
+            String deadlineString = (elements[3]);
             deadlineString = String.format("%s-%s-%s %s:%s", deadlineString.split("-")[0],
                     deadlineString.split("-")[1], deadlineString.split("-")[2],
                     deadlineString.split("-")[3], deadlineString.split("-")[4]);
             DateTimeFormatter deadlineFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
             LocalDateTime deadline = LocalDateTime.parse( deadlineString, deadlineFormat );
-            int duration = Integer.parseInt((quizList.get(i).split(":"))[4]);
-            int totalPoints = Integer.parseInt((quizList.get(i).split(":"))[5]);
+            int duration = Integer.parseInt(elements[4]);
+            int totalPoints = Integer.parseInt(elements[5]);
             int[] questionsIndex = new int[numberQuestions];
             for ( int j = 0 ; j < numberQuestions ; j++ ) {
-                questionsIndex[j] = Integer.parseInt((quizList.get(i).split(":"))[6].split(",")[j]);
+                questionsIndex[j] = Integer.parseInt(elements[6].split(",")[j]);
             }
             Quiz quiz = new Quiz(quizID, numberQuestions, deadline,
                     duration, totalPoints, questionsIndex, questionPoints);
@@ -58,7 +53,6 @@ public class Student extends Account {
         }
         return quizzes;
     }
-
 
     /** Take desired quiz and save user response in string array
      * CS1800 Spring 2022, Project 4
@@ -81,115 +75,65 @@ public class Student extends Account {
      * @author Alex Lee
      * @version 4/9/2022
      */
-    public Quiz loadQuiz( int QuizID ) {
+    public Quiz loadQuiz( int wantedID ) {
         Quiz[] quizzes = retrieveQuizzes();
         Quiz quiz = null;
         for ( int i = 0 ; i < quizzes.length ; i++ ) {
-            if ( quizzes[i].getQuizID() == QuizID ) {
+            if ( quizzes[i].getQuizID() == wantedID ) {
                 quiz = quizzes[i];
             }
         }
         if ( quiz == null ) {
             System.out.println("The quiz doesn't exist!");
         } else {
-            System.out.printf("Quiz %d loaded!\n", QuizID);
+            System.out.printf("Quiz %d:\n", wantedID);
         }
         return quiz;
     }
 
-    /** Load the current grades from grade file and return it in the format "QUIZ ID: Grade"
+    /** Load the current grades from grade file and return it in the format "grade,timestamp(YYYY-MM-DD HH:SS)"
      * CS1800 Spring 2022, Project 4
      * @author Alex Lee
      * @version 4/11/2022
      */
-    public String loadGrade( int QuizID) {
+    public String loadGrade(int wantedID) {
         String grade = null;
-        ArrayList<String> grades= new ArrayList<String>();
-        try (BufferedReader bfr = new BufferedReader(new FileReader("Grades.txt"))) {
-            String line;
-            while ((line = bfr.readLine()) != null) {
-                grades.add(line);
-            }
-        } catch (IOException ie) {
-            System.out.println("Either the file doesn't exist or the file is in the wrong format!");
+        String[] timestampElements;
+        String gradeFile = tools.loadTextFile("Grades.txt");
+        String[] lines = gradeFile.split("/");
+
+        boolean exists = false;
+        String[] usernames = new String[lines.length];
+        for ( int i = 0 ; i < lines.length ; i ++ ) {
+            usernames[i] = lines[i].split("-")[0];
         }
-
-        String entireFile = "";
-        for ( int i = 0 ; i < grades.size() ; i++ ) {
-            entireFile += grades.get(i);
-        }
-
-        String[] allGrades = entireFile.split("/");
-
-        String[] usernames = new String[allGrades.length];
-        String[] studentsGrades = new String[allGrades.length];
-        for ( int i = 0 ; i < allGrades.length ; i ++ ) {
-            usernames[i] = allGrades[i].split("-")[0];
-            studentsGrades[i] = allGrades[i].split("-")[1];
-        }
-
         int studentIndex = 0;
         for ( int i = 0 ; i < usernames.length ; i++ ) {
             if ( usernames[i].equals(this.getUsername())) {
                 studentIndex = i;
+                exists = true;
             }
         }
-
-        int quizIndex = 0;
-        boolean quizTaken = false;
-        String[] thisStudentGrades = studentsGrades[studentIndex].split(",");
-        for ( int i = 0 ; i < thisStudentGrades.length ; i++ ) {
-            String quizID = thisStudentGrades[i].split(":")[0];
-            if ( Integer.parseInt(quizID) == QuizID ) {
-                quizIndex = i;
-                quizTaken = true;
+        if (exists) {
+            int quizIndex = 0;
+            boolean quizTaken = false;
+            String[] thisStudentGrades = lines[studentIndex].split("-")[1].split(",");
+            for ( int i = 1 ; i < thisStudentGrades.length ; i++ ) {
+                String quizID = thisStudentGrades[i].split(":")[0];
+                if ( Integer.parseInt(quizID) == wantedID ) {
+                    quizIndex = i;
+                    quizTaken = true;
+                }
             }
-        }
-        if ( quizTaken ) {
-            grade = (thisStudentGrades[quizIndex].split(":"))[1];
+            if ( quizTaken ) {
+                grade = (thisStudentGrades[quizIndex].split(":"))[1];
+                timestampElements = ((thisStudentGrades[quizIndex].split(":"))[2]).split(" ");
+                String deadlineString = String.format("%s-%s-%s %s:%s", timestampElements[0], timestampElements[1],
+                        timestampElements[2], timestampElements[3], timestampElements[4]);
+                System.out.printf("Quiz %d submitted %s\n", wantedID, deadlineString);
+            }
         }
         return grade;
-    }
-
-    /** Load past grades by this student
-     * CS1800 Spring 2022, Project 4
-     * @author Alex Lee
-     * @version 4/11/2022
-     */
-    public String loadPastGrade() {
-        String pastGrades = null;
-        ArrayList<String> grades= new ArrayList<String>();
-        try (BufferedReader bfr = new BufferedReader(new FileReader("Grades.txt"))) {
-            String line;
-            while ((line = bfr.readLine()) != null) {
-                grades.add(line);
-            }
-        } catch (IOException ie) {
-            System.out.println("Either the file doesn't exist or the file is in the wrong format!");
-        }
-
-        String entireFile = "";
-        for ( int i = 0 ; i < grades.size() ; i++ ) {
-            entireFile += grades.get(i);
-        }
-
-        String[] allGrades = entireFile.split("/");
-
-        String[] usernames = new String[allGrades.length];
-        String[] studentsGrades = new String[allGrades.length];
-        for ( int i = 0 ; i < allGrades.length ; i ++ ) {
-            usernames[i] = allGrades[i].split("-")[0];
-            studentsGrades[i] = allGrades[i].split("-")[1];
-        }
-
-        int studentIndex = 0;
-        for ( int i = 0 ; i < usernames.length ; i++ ) {
-            if ( usernames[i].equals(this.getUsername())) {
-                studentIndex = i;
-            }
-        }
-        pastGrades = studentsGrades[studentIndex];
-        return pastGrades;
     }
 
 
@@ -198,84 +142,103 @@ public class Student extends Account {
      * @author Alex Lee
      * @version 4/9/2022
      */
-    public void recordGrade( float grade, String username, int quizID) {
-        ArrayList<String> grades = new ArrayList<String>();
-        try (BufferedReader bfr = new BufferedReader(new FileReader("Grades.txt"))) {
-            String line;
-            while ((line = bfr.readLine()) != null) {
-                grades.add(line);
-            }
-        } catch (IOException ie) {
-            System.out.println("Either the file doesn't exist or the file is in the wrong format!");
-        }
-
-        String entireFile = null;
-        String[] usernames = new String[0];
-        boolean studentExists = false;
-        String[] allGrades = new String[0];
-        if (grades.size() > 0) {
-            entireFile = "";
-            for (int i = 0; i < grades.size(); i++) {
-                entireFile += grades.get(i);
-            }
-            allGrades = entireFile.split("/");
-            usernames = new String[allGrades.length];
-            for (int i = 0; i < allGrades.length; i++) {
-                usernames[i] = allGrades[i].split("-")[0];
-            }
-            int studentIndex = 0;
-            studentExists = false;
-            for (int i = 0; i < usernames.length; i++) {
-                if (usernames[i].equals(this.getUsername())) {
-                    studentIndex = i;
-                    studentExists = true;
-                }
-            }
-
-        }
-        String[] updatedGrades;
-        if (studentExists) {
-            String pastGrades = loadPastGrade();
-            String[] pastQuizGrades = pastGrades.split(",");
-            int[] pastQuizID = new int[pastQuizGrades.length];
-            for (int i = 0; i < pastQuizGrades.length; i++) {
-                pastQuizID[i] = Integer.parseInt((pastQuizGrades[i].split(":"))[0]);
-            }
-            int quizIndex;
-            boolean taken = false;
-            for (int i = 0; i < pastQuizID.length; i++) {
-                if (pastQuizID[i] == quizID) {
-                    quizIndex = i;
-                    taken = true;
-                }
-            }
-            String updatedGrade = "";
-            if (taken) {
-                System.out.println("You have already taken this quiz before. Please contact your teacher!");
-            } else {
-                updatedGrade = pastGrades.substring(0, pastGrades.length() - 1);
-                updatedGrade += String.format(",%d:%.2f/", quizID, grade);
-            }
-            int studentIndex = 0;
-            allGrades[studentIndex] = getUsername() + "-" + updatedGrade;
-            updatedGrades = allGrades;
+    public void recordGrade(float grade, int wantedID, LocalDateTime timestamp) {
+        String gradeFile = tools.loadTextFile("Grades.txt");
+        String[] lines = gradeFile.split("/");
+        boolean hasTaken = hasTaken(wantedID);
+        boolean exists = false;
+        if (hasTaken) {
+            System.out.println("ERROR: You have already taken this quiz before!");
         } else {
-            updatedGrades = new String[allGrades.length + 1];
-            for (int i = 0; i < allGrades.length; i++) {
-                updatedGrades[i] = allGrades[i];
+            String[] usernames = new String[lines.length];
+            int lineIndex = 0;
+            for ( int i = 0 ; i < usernames.length ; i++) {
+                usernames[i] = lines[i].split("-")[0];
             }
-            updatedGrades[updatedGrades.length - 1] = String.format("%s-%d:%.2f", username, quizID, grade);
+            for (int i = 0 ; i < usernames.length; i++) {
+                if (getUsername().equals(usernames[i])) {
+                    lineIndex = i;
+                    exists = true;
+                }
+            }
+            if (exists) {
+                lines[lineIndex] = lines[lineIndex] + "," + String.format("%d:%.2f:%d %d %d %d %d", wantedID, grade,
+                        timestamp.getYear(), timestamp.getMonthValue(), timestamp.getDayOfMonth(),
+                        timestamp.getHour(), timestamp.getMinute());
+                try {
+                    out = new PrintWriter(new FileOutputStream("Grades.txt", false));
+                    for (int i = 0; i < lines.length; i++) {
+                        out.println(lines[i] + "/");
+                    }
+                    out.close();
+                    System.out.println("Grade recorded!");
+                } catch (Exception e) {
+                    System.out.println("ERROR RECORDING GRADE");
+                }
+                System.out.println("TEST POINT 2");
+            } else {
+                System.out.println("TEST POINT 3");
+                String[] newLines = new String[lines.length + 1];
+                for (int i = 1 ; i < lines.length ; i++) {
+                    newLines[i] = lines[i];
+                }
+                newLines[newLines.length - 1] = String.format("%s-,%d:%.2f:%d %d %d %d %d", getUsername(), wantedID,
+                        grade, timestamp.getYear(), timestamp.getMonthValue(), timestamp.getDayOfMonth(),
+                        timestamp.getHour(), timestamp.getMinute());
+                try {
+                    out = new PrintWriter(new FileOutputStream("Grades.txt", false));
+                    for (int i = 0; i < newLines.length; i++) {
+                        if (newLines[i] != null) {
+                            out.println(newLines[i] + "/");
+                        }
+                    }
+                    out.close();
+                    System.out.println("Grade recorded!");
+                } catch (Exception e) {
+                    System.out.println("ERROR RECORDING GRADE");
+                }
+            }
+        }
+    }
+
+    /** Checks to see if the student has taken the quiz before
+     * CS1800 Spring 2022, Project 4
+     * @author Alex Lee
+     * @version 4/16/2022
+     */
+    public boolean hasTaken(int wantedID) {
+        boolean hasTaken = false;
+        boolean userExists = false;
+        String gradeFile = tools.loadTextFile("Grades.txt");
+        String[] lines = gradeFile.split("/");
+        System.out.printf("TEST LINES LENGTH: %d\n", lines.length);
+        if (lines.length > 1) {
+            String[] usernames = new String[lines.length];
+            int lineIndex = 0;
+            for (int i = 0 ; i < usernames.length ; i++) {
+                usernames[i] = lines[i].split("-")[1];
+            }
+            for (int i = 0 ; i < usernames.length ; i++) {
+                if (this.getUsername().equals(usernames[i])) {
+                    userExists = true;
+                    lineIndex = i;
+                    break;
+                }
+            }
+            if (userExists) {
+                String[] quizzes = (lines[lineIndex].split("-"))[1].split(",");
+                String[] takenIDs = new String[quizzes.length];
+                for ( int i= 0 ; i < quizzes.length ; i++) {
+                    takenIDs[i] = quizzes[i].split(":")[0];
+                }
+                for (int i = 0 ; i < takenIDs.length ; i++) {
+                    if (wantedID == Integer.parseInt(takenIDs[i])) {
+                        hasTaken = true;
+                    }
+                }
+        }
         }
 
-        try {
-            out = new PrintWriter(new FileOutputStream("Grades.txt", false));
-            for (int i = 0; i < updatedGrades.length; i++) {
-                out.println(updatedGrades[i]);
-            }
-            out.close();
-            System.out.println("Grade recorded!");
-        } catch (Exception e) {
-            System.out.println("ERROR RECORDING GRADE");
-        }
+        return hasTaken;
     }
 }
