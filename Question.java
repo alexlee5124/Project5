02b4c740fbe0a4ca4d184090a2913ad6a4861827
alexlee5124 <59184872+alexlee5124@users.noreply.github.com>
2022-04-16 11:12:35 -0400

    Update Question.java

--- Question.java
+++ Question.java
@@ -12,25 +12,37 @@ public class Question {
     private int pointValue;
     private String prompt;
     private String answer;
+    private String questionType;    // Question type: M for multiple choice, R for response or True/False
 
     /** Getter & Setter, Fields, Constructors
      * CS1800 Spring 2022, Project 4
      * @author Taylor Graham, Alex Lee
      * @version 4/5/2022
      */
-    public Question(int pointValue, String prompt, String answer) {
+    public Question(int pointValue, String prompt, String answer, String questionType) {
         this.pointValue = pointValue;
         this.prompt = prompt;
         this.answer = answer;
+        this.questionType = questionType;
     }
-    Question() {
+    public Question(String prompt, String answer, String questionType) {
         this.pointValue = 0;
-        this.prompt = null;
-        this.answer = null;
+        this.prompt = prompt;
+        this.answer = answer;
+        this.questionType = questionType;
     }
 
     public String getPrompt() {
-        return this.prompt;
+        if (this.questionType.equals("R")) {
+            return this.prompt;
+        } else {
+            String[] promptElements = this.prompt.split(":");
+            String prompt = promptElements[0];
+            for (int i = 1 ; i < promptElements.length ; i++) {
+                prompt += String.format("\n%d. %s", i, promptElements[i]);
+            }
+            return prompt;
+        }
     }
 
     public String getAnswer() {
@@ -59,8 +71,8 @@ public class Question {
      */
     public int gradeQuestion( String response ) {
         int pointsEarned = 0;
-        if (response.equals(this.answer.toLowerCase())) {
-            pointsEarned = this.pointValue;
+        if (response.equalsIgnoreCase(getAnswer())) {
+            pointsEarned = getPointValue();
         }
         return pointsEarned;
     }
@@ -70,8 +82,8 @@ public class Question {
      * @author Alex Lee
      * @version 4/9/2022
      */
-    public String toString() {
-        return String.format("%s,%s", this.prompt, this.answer);
+    public String toString(){
+        return String.format("%s,%s,%s/", this.prompt, this.answer, this.questionType);
     }
 
 }
